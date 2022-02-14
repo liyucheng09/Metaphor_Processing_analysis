952ab088efe5a8cc7b01005eaf8c9133c52657b4
liyucheng <liyucheng_2@qq.com>
2022-02-14 07:31:15 +0000

    add moh dataset to roberta_seq scripts

--- roberta_seq.py
+++ roberta_seq.py
@@ -44,68 +44,70 @@ def tokenize_alingn_labels_replace_with_mask_and_add_type_ids(ds, do_mask=False)
 
 if __name__ == '__main__':
 
-    model_name, data_dir, = sys.argv[1:]
-    save_folder = '/vol/research/nlg/frame_finder/'
+    model_name, data_dir, dataset_name = sys.argv[1:]
+    save_folder = '/vol/research/nlg/metaphor/'
     # save_folder = './'
-    output_dir = os.path.join(save_folder, 'checkpoints/roberta_seq/')
+    output_dir = os.path.join(save_folder, f'checkpoints/{dataset_name}/roberta_seq/')
     logging_dir = os.path.join(save_folder, 'logs/')
     prediction_output_file = os.path.join(output_dir, 'error_instances.csv')
 
     tokenizer = get_tokenizer(model_name, add_prefix_space=True)
+    p = get_hf_ds_scripts_path(dataset_name)
 
-    p = get_hf_ds_scripts_path('vua20')
-    data_files={'train': os.path.join(data_dir, 'train.tsv'), 'test': os.path.join(data_dir, 'test.tsv')}
+    if dataset_name == 'moh':
+        data_files=data_dir
+        ds = datasets.load_dataset(p, data_files=data_files)
+        ds = ds.map(tokenize_alingn_labels_replace_with_mask_and_add_type_ids)
+        ds = ds['train'].train_test_split(test_size=0.12)
+    elif dataset_name == 'vua20':
+        data_files={'train': os.path.join(data_dir, 'train.tsv'), 'test': os.path.join(data_dir, 'test.tsv')}
+        ds = get_tokenized_ds(p, tokenizer, tokenize_func='tagging', \
+            tokenize_cols=['tokens'], tagging_cols={'is_target':0, 'labels':-100}, \
+            data_files=data_files, batched=False, name = 'combined')
 
-    # ds = datasets.load_dataset(p, data_files=data_files, split='test[:20]')
-    # ds = ds.map(tokenize_alingn_labels_replace_with_mask_and_add_type_ids)
-    ds = get_tokenized_ds(p, tokenizer, tokenize_func='tagging', \
-        tokenize_cols=['tokens'], tagging_cols={'is_target':0, 'labels':-100}, \
-        data_files=data_files, batched=False, split='test[:20%]', name = 'combined')
     # ds.rename_column_('target_mask', 'token_type_ids')
+    # ds = ds.rename_column('is_target', 'token_type_ids')
 
-    ds = ds.rename_column('is_target', 'token_type_ids')
-
-    # dl = get_dataloader(ds, cols=['attention_mask', 'input_ids', 'labels'])
     args = get_base_hf_args(
         output_dir=output_dir,
         logging_steps=1,
         logging_dir = logging_dir,
         lr=5e-5,
         train_batch_size=24,
+        epochs=5
     )
 
     data_collator = DataCollatorForTokenClassification(tokenizer, max_length=128)
-    model = get_model(RobertaForTokenClassification, model_name, num_labels = 2, type_vocab_size=2)
-    # model = get_model(RobertaForTokenClassification, model_name, num_labels = 2,)
+    # model = get_model(RobertaForTokenClassification, model_name, num_labels = 2, type_vocab_size=2)
+    model = get_model(RobertaForTokenClassification, model_name, num_labels = 2,)
     # model.roberta.embeddings.token_type_embeddings = torch.nn.Embedding(2, 768)
     # model._init_weights(model.roberta.embeddings.token_type_embeddings)
 
     trainer = Trainer(
         model=model,
         args=args,
-        # train_dataset=ds['train'],
-        # eval_dataset=ds['test'],
+        train_dataset=ds['train'],
         data_collator=data_collator,
         tokenizer=tokenizer,
         callbacks=[TensorBoardCallback()],
         compute_metrics=tagging_eval_for_trainer
     )
 
-    # trainer.train()
-    # trainer.save_model()
+    trainer.train()
+    trainer.save_model()
 
-    # result = trainer.evaluate()
-    # print(result)
+    result = trainer.evaluate()
+    print(result)
 
     # pred_out = trainer.predict(ds['test'])
-    pred_out = trainer.predict(ds)
+    # pred_out = trainer.predict(ds)
 
-    predictions = pred_out.predictions
-    labels = pred_out.label_ids
-    predictions = np.argmax(predictions, axis=-1)
+    # predictions = pred_out.predictions
+    # labels = pred_out.label_ids
+    # predictions = np.argmax(predictions, axis=-1)
 
-    true_p, true_l = get_true_label(predictions, labels)
-    show_error_instances_id(true_p, true_l, prediction_output_file, ds['sent_id'], ds['tokens'])
+    # true_p, true_l = get_true_label(predictions, labels)
+    # show_error_instances_id(true_p, true_l, prediction_output_file, ds['sent_id'], ds['tokens'])
     # write_predict_to_file(pred_out, ds['tokens'], out_file=prediction_output_file)
     # result = eval_with_weights(pred_out, ds['test']['token_type_ids'])
     # print(result)
